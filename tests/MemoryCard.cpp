f113a85c5f94a9072792169d9527604d790d6508
Joshua Saxby <joshua.a.saxby@gmail.com>
2021-04-28 03:12:50 +0100

    Reorganise tests, convert Card ID test to BDD-style

--- tests/MemoryCard.cpp
+++ tests/MemoryCard.cpp
@@ -7,30 +7,6 @@
 #include <ps1-memcard-protocol/MemoryCard.hpp>
 
 using namespace com::saxbophone::ps1_memcard_protocol;
-// test case to check network construction and train movement
-TEST_CASE("Get Memory Card ID Command") {
-    MemoryCard card;
-    // sequence of command bytes to get memory card ID
-    std::optional<std::uint8_t> inputs[] = {
-        0x81, 0x53, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-    };
-    // sequence of expected response bytes that card should respond with
-    std::optional<std::uint8_t> expected_outputs[] = {
-        std::nullopt, 0x08, 0x5A, 0x5D, 0x5C, 0x5D, 0x04, 0x00, 0x00, 0x80,
-    };
-
-    // power up the card
-    REQUIRE(card.power_on());
-    // send each command byte and check the response in turn
-    for (std::size_t i = 0; i < 10; i++) {
-        std::optional<std::uint8_t> output = std::nullopt;
-        CHECK(card.send(inputs[i], output)); // check card ACK
-        // now check the response was as expected
-        REQUIRE(output == expected_outputs[i]);
-    }
-    // power down the card
-    REQUIRE(card.power_off());
-}
 
 SCENARIO("MemoryCard can be powered on when off and off when on") {
     GIVEN("A MemoryCard that is powered off (default state)") {
@@ -51,5 +27,34 @@ SCENARIO("MemoryCard can be powered on when off and off when on") {
                 }
             }
         }
+    }
+}
+
+SCENARIO("Get ID Command can be sent to Memory Card with correct response") {
+    GIVEN("A MemoryCard that is powered on") {
+        MemoryCard card;
+        // power up the card
+        REQUIRE(card.power_on());
+        AND_GIVEN("A sequence of command bytes to get memory card ID") {
+            std::optional<std::uint8_t> inputs[] = {
+                0x81, 0x53, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+            };
+            AND_GIVEN("A sequence of data bytes the card is expected to respond with") {
+                std::optional<std::uint8_t> expected_outputs[] = {
+                    std::nullopt, 0x08, 0x5A, 0x5D, 0x5C, 0x5D, 0x04, 0x00, 0x00, 0x80,
+                };
+                WHEN("The sqeuence of command bytes is sent to the card") {
+                    THEN("The card responds with the expected response byte") {
+                        for (std::size_t i = 0; i < 10; i++) {
+                            std::optional<std::uint8_t> output = std::nullopt;
+                            CHECK(card.send(inputs[i], output)); // check card ACK
+                            CHECK(output == expected_outputs[i]);
+                        }
+                    }
+                }
+            }
+        }
+        // power down the card
+        REQUIRE(card.power_off());
     }
 }
