cc90ba80b4e3b740352d38da6f4d88a7ca662a73
Joshua Saxby <joshua.a.saxby@gmail.com>
2021-05-01 18:53:35 +0100

    Add empty slot send test case and add missing checks to other case

--- tests/MemoryCardSlot.cpp
+++ tests/MemoryCardSlot.cpp
@@ -11,7 +11,7 @@
 using namespace com::saxbophone::ps1_memcard_protocol;
 
 SCENARIO("MemoryCards can be inserted and removed from MemoryCardSlot") {
-    GIVEN("A MemoryCardSlot") {
+    GIVEN("An empty MemoryCardSlot") {
         MemoryCardSlot slot;
         THEN("Attempting to remove a card from the empty slot fails") {
             CHECK_FALSE(slot.remove_card());
@@ -21,14 +21,31 @@ SCENARIO("MemoryCards can be inserted and removed from MemoryCardSlot") {
             THEN("The card can be inserted into the slot successfully") {
                 REQUIRE(slot.insert_card(card));
                 AND_WHEN("The slot has had a card inserted into it") {
+                    THEN("The card should be powered on") {
+                        CHECK(card.powered_on);
+                    }
                     THEN("Attempting to insert a card into the slot fails") {
                         CHECK_FALSE(slot.insert_card(card)); // doesn't matter that it's the same card
                     }
                     THEN("The card can be removed from the slot successfully") {
-                        CHECK(slot.remove_card());
+                        REQUIRE(slot.remove_card());
+                        AND_THEN("The card should be powered off") {
+                            CHECK_FALSE(card.powered_on);
+                        }
                     }
                 }
             }
+        }
+    }
+}
+
+SCENARIO("Calling MemoryCardSlot.send() with no MemoryCard inserted") {
+    GIVEN("An empty MemoryCardSlot") {
+        MemoryCardSlot slot;
+        THEN("Calling send() method of the slot always returns false") {
+            std::uint8_t command = GENERATE(take(100, random(0x00, 0xFF)));
+            std::optional<std::uint8_t> result;
+            REQUIRE_FALSE(slot.send(command, result));
         }
     }
 }
